f49d3c1c8a2a3c96cd1c704304fb255ef1ea30ab
njavor <nikolettjavor@gmail.com>
2023-07-11 02:21:23 +0200

    yes

---
+++ static/js/nameIn.js
@@ -0,0 +1,15 @@
+const title = document.getElementById('logolink');
+const asubhead = document.getElementById('subhead');
+
+window.addEventListener('scroll', function(){
+    if(title.classList.contains('show')){
+        if(this.scrollY < asubhead.offsetTop){
+        title.classList.remove('show');
+        }
+    }
+    else{
+        if( asubhead.offsetTop <= this.scrollY){
+        title.classList.add('show');
+        }
+    }
+})
